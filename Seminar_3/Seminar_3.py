f0493db3bb1c7ad01b361dfbf88e4c6f197ab343
Nikita <ezio13@yandex.ru>
2022-11-26 17:56:42 +0300

    ex 1-2 done

--- Seminar_3/Seminar_3.py
+++ Seminar_3/Seminar_3.py
@@ -1,5 +1,39 @@
 try:
-    ex_num = int(input('Введите номер задачи'))
+    ex_num = int(input('Введите номер задачи '))
+
+    if ex_num == 1:
+
+        try:
+            lst = [float(i) for i in input('Введите массив чисел через пробел: ').split()]
+
+            summary = 0
+            for i in range(1, len(lst), 2):
+                summary += lst[i]
+
+            print(f'Сумма нечетных элементов массива равна {summary}')
+        except:
+            print('Числа. Через. Пробел!')
+
+    if ex_num == 2:
+
+        try:
+            lst = [float(i) for i in input('Введите массив чисел через пробел: ').split()]
+
+            new_lst = []
+
+            for i, el in enumerate(lst):
+                if i >= len(lst) / 2:
+                    break
+                else:
+                    new_lst.append(el * lst[len(lst)-1-i])
+            print(*new_lst)
+        except:
+            print('Числа. Через. Пробел!')
+
+
+
+
+
 
 
 except:
