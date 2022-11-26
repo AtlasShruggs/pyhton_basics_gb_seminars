681cdab3dbec345025316bd28098d9aeb72c50ca
Nikita <ezio13@yandex.ru>
2022-11-26 18:08:39 +0300

    ex 3 done

--- Seminar_3/Seminar_3.py
+++ Seminar_3/Seminar_3.py
@@ -30,6 +30,27 @@ try:
         except:
             print('Числа. Через. Пробел!')
 
+    if ex_num == 3:
+
+        try:
+            lst = [float(i) for i in input('Введите массив чисел через пробел: ').split()]
+
+            parts = [str(i).split('.')[1] for i  in lst ]
+
+            parts_max_len = len(max(parts, key=len))
+
+            parts_int = []
+            for el in parts:
+                parts_int.append(int(el + '0' * (parts_max_len - len(el))))
+        
+            diff = float('0.' + str(max(parts_int) - min(parts_int)))
+            print(diff)
+
+
+        except:
+            print('Числа. Через. Пробел!')
+
+
 
 
 
