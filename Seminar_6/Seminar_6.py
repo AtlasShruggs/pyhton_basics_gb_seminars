fcc75a05e4088f1e35505ba0cbe9d6f69e03e465
Nikita <ezio13@yandex.ru>
2022-12-12 18:56:54 +0300

    ex 2 done

--- Seminar_6/Seminar_6.py
+++ Seminar_6/Seminar_6.py
@@ -2,13 +2,33 @@ from functools import reduce
 
 try:
     ex_num = int(input('Введи номер задачи: '))
+except:
+    print('Такой задачи нет. Попробуй еще раз')
 
-    if ex_num == 1:
-        num_list = [int(i) for i in input('Введи целые числа через пробел: ').split()]
+
+if ex_num == 1:
+    num_list = [int(i) for i in input('Введи целые числа через пробел: ').split()]
 
     two_digits_list = filter(lambda x: x // 10 and not x // 100, num_list)
 
     print(*two_digits_list, sep=' ')
 
-except:
-    print('Такой задачи нет. Попробуй еще раз')
+if ex_num == 2:
+
+    work_list = [i for i in input('Введи разные штуки через пробел: ').split()]
+
+    digits = filter(lambda x: x.isdigit(), work_list)
+
+    letters = filter(lambda x: x.isalpha(), work_list)
+
+    digits, letters = list(map(list,(digits, letters)))
+
+    combined = [el for el in work_list if el not in digits+letters]
+
+    
+
+
+    print(*letters)
+    print(*digits)
+    print(*combined)
+
